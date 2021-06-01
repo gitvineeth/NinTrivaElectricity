9057f24a79e6791d03afa5f2151933dcd1229dab
Vineet <amvineethcalicut@gmail.com>
2021-06-01 13:14:00 +0530

    Added endpoint for retrieving bill

--- src/main/java/com/electricity/nintriva/controller/BillController.java
+++ src/main/java/com/electricity/nintriva/controller/BillController.java
@@ -1,6 +1,8 @@
 package com.electricity.nintriva.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.electricity.nintriva.model.BillInputDetails;
 import com.electricity.nintriva.service.BillService;
+
 
 @RestController
 @RequestMapping("/bill")
@@ -19,11 +22,15 @@ public class BillController {
 	@PostMapping(value="/submit")
 	public void submitBill(@RequestBody BillInputDetails billInputDetails) { 
 		
-		billService.submitBill(billInputDetails);
-		
-		
+	  billService.submitBill(billInputDetails);
+	}
 	
+	
+	@GetMapping("/get/{consumerNumber}")
+	public int getUserBill(@PathVariable String consumerNumber) { 
 		
+		int consumerNo = Integer.parseInt(consumerNumber);
+		return consumerNo;
 	}
 	
 	
